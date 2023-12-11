82a107b71e736bfbb08aa0348c76b552b66780c2
John Flinchbaugh <john@hjsoft.com>
2023-12-11 09:01:07 -0500

    day 5, part 2 incomplete

--- src/aoc2023/day_05.clj
+++ src/aoc2023/day_05.clj
@@ -42,8 +42,58 @@
       (apply min))
     ))
 
+(defn- expand-seed [[start length]]
+  [start (+ start length -1)])
+
+(defn- expand-seeds [seeds]
+  (->>
+    seeds
+    (partition 2)
+    (map expand-seed)))
+
+(defn- expand-mapping [[dest src length]]
+  [[src (+ src length -1)] [dest (+ dest length -1)]])
+
+(defn- intersects? [[s1 e1] [s2 e2]]
+  (or
+    (<= s1 s2 e1)
+    (<= s2 s1 e2)))
+
+(defn part-2 []
+  (let [input
+        (->>
+          "src/aoc2023/day_05.txt"
+          file->lines
+          (map (fn [s] (str/replace s #"seeds: " "")))
+          (remove (partial re-matches #".*map:"))
+          (map split-line)
+          (partition-by #{[nil]})
+          (remove #{[[nil]]})
+          )
+        seeds (expand-seeds (ffirst input))
+        mappings (->>
+                   input
+                   rest
+                   (map (fn [m]
+                          (->>
+                            m
+                            (map expand-mapping)
+                            (sort-by (fn [[[ss se] [ds de]]] ds)))))
+                   reverse)]
+    (->> mappings
+      first
+      first
+      first
+      )
+    ))
+
 (comment
   (part-1)
   ;; => 289863851
+  ;; => 289863851
+
+
+  (part-2)
+
 
   nil)
