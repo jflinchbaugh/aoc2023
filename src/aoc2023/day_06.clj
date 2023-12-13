daade37607145f1dc3cde37d1bc194656796c37c
John Flinchbaugh <john@hjsoft.com>
2023-12-13 00:25:59 -0500

    day 6 part 1

---
+++ src/aoc2023/day_06.clj
@@ -0,0 +1,45 @@
+(ns aoc2023.day-06
+  (:require [aoc2023.core :refer :all]
+            [clojure.string :as str]))
+
+
+(def input (str/trim "
+Time:        61     70     90     66
+Distance:   643   1184   1362   1041
+"))
+
+
+(defn distance [t a]
+  (* a (- t a)))
+
+(def races [
+            [61 643]
+            [70 1184]
+            [90 1362]
+            [66 1041]])
+
+
+(defn part-1 []
+  (->>
+    races
+    (map
+      (fn [[t r]]
+        (->>
+          t
+          inc
+          range
+          (map (partial distance t))
+          (filter (partial < r))
+          count
+          )))
+    (reduce * 1)
+    )
+  )
+
+
+(comment
+
+  (part-1)
+  ;; => 293046
+
+  )
