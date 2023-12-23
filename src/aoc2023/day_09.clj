348455c969b67770f7a9bf382b6261c8454a98c5
John Flinchbaugh <john@hjsoft.com>
2023-12-22 20:55:38 -0500

    day 9, part 2

--- src/aoc2023/day_09.clj
+++ src/aoc2023/day_09.clj
@@ -18,6 +18,14 @@
     (map last)
     (reduce +)))
 
+(defn- find-prev-value [lst]
+  (->> lst
+    (iterate solve)
+    (take-while (complement (partial every? zero?)))
+    (map first)
+    reverse
+    (reduce (fn [a b] (- b a)) 0)))
+
 (defn part-1 []
   (->>
     "src/aoc2023/day_09.txt"
@@ -26,9 +34,19 @@
     (map find-next-value)
     (reduce +)))
 
+(defn part-2 []
+  (->>
+    "src/aoc2023/day_09.txt"
+    file->lines
+    (map parse)
+    (map find-prev-value)
+    (reduce +)))
 
 (comment
   (part-1)
   ;; => 1882395907
+
+  (part-2)
+  ;; => 1005
 
   nil)
